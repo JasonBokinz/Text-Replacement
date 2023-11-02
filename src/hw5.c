be37c35ce46d5d74a206f9ab43fcea2e99fd8422
Jason Bokinz <jasonbokinz@Jasons-MacBook-Pro.local>
2023-11-02 16:45:20 -0400

    final hw5.c

--- src/hw5.c
+++ src/hw5.c
@@ -1,9 +1,10 @@
 #include "../include/hw5.h"
-
+/* This method is used to handle the errors based on their priority. */
 int handleError(int i) {
     int errors[8] = {MISSING_ARGUMENT, DUPLICATE_ARGUMENT, INPUT_FILE_MISSING, OUTPUT_FILE_UNWRITABLE, S_ARGUMENT_MISSING, R_ARGUMENT_MISSING, L_ARGUMENT_INVALID, WILDCARD_INVALID};
     return errors[i];
 }
+/* This method is used to find the beginning of a word when the wildcard endsWith is found. */
 int findBeginning(char* tmp, char* pos) {
     int count = 0; // Initialize a count to track the number of characters moved
     while (pos > tmp && !isspace(*(pos - 1)) && !ispunct(*(pos - 1))) {
@@ -12,6 +13,7 @@ int findBeginning(char* tmp, char* pos) {
     }
     return count;
 }
+/* This method is used to check if the word that was found matches the wildcard endsWith */
 bool checkIsEnding(char* pos, char* word) {
     pos += strlen(word) - 1;
     if (*(pos+1) != '\0') {
@@ -20,6 +22,7 @@ bool checkIsEnding(char* pos, char* word) {
         return true;
     }
 }
+/* This method is used to check if the word that was found matches the wildcard startsWith */
 bool checkIsBeginning(char* tmp, char* pos) {
         if (pos - 1 >= tmp) {
             return isspace(*(pos-1)) || ispunct(*(pos-1));
@@ -27,23 +30,37 @@ bool checkIsBeginning(char* tmp, char* pos) {
             return true;
         }
 }
-// Have an error where each index is an error and return the error at the very end
+/* This method counts the number of asterisks in a string. */
+int countAsterisks(char *str) {
+    char* ptr = str;
+    int count = 0;
+    while (*ptr) {
+        if (*ptr == '*') {
+            count++;
+        }
+        ptr++;
+    }
+    return count;
+}
+/* Main method of the code */
 int main(int argc, char *argv[]) {
     bool isWildcard = false;
     bool hasLine = false;
     bool priorityError[9] = {false};
     int numS = 0, numR = 0, numL = 0, numW = 0;
+    /* Checks for missing arguments error. */
     if (argc < 7) {
         priorityError[0] = true;
     }
-    for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
-    }
+
     char* search_text = NULL, *replace_text = NULL;
     int start_line = 0, end_line = 0;
+    /* Loops through each argv string given by the user. */
     for (int i = 0; i < argc; i++) {
+        /* Checks for duplicate argument error. */
         if ((numS > 1) || (numR > 1) || (numL > 1) || (numW > 1)) {
             priorityError[1] = true;
+        /* agv= -s: store search_text and check if the s argument is missing */
         } else if (strcmp(argv[i], "-s") == 0) {
             numS++;
             if ((i + 1 < argc) && (strncmp(argv[i + 1], "-", 1) != 0)) {
@@ -51,6 +68,7 @@ int main(int argc, char *argv[]) {
             } else {
                 priorityError[4] = true;
             }
+        /* agv= -r: store replace_text and check if the r argument is missing */
         } else if (strcmp(argv[i], "-r") == 0){
             numR++;
             if ((i + 1 < argc) && (strncmp(argv[i + 1], "-", 1) != 0)) {
@@ -58,6 +76,7 @@ int main(int argc, char *argv[]) {
             } else {
                 priorityError[5] = true;
             }
+        /* agv= -l: store start_line/end_line and check if the l argument is invalid */
         } else if (strcmp(argv[i], "-l") == 0) {
             numL++;
             if ((i + 1 < argc) && (strncmp(argv[i + 1], "-", 1) != 0)) {
@@ -82,20 +101,25 @@ int main(int argc, char *argv[]) {
             } else {
                 priorityError[6] = true;
             }
+        /* agv= -w: the -s argument is a wildcard */
         } else if (strcmp(argv[i], "-w") == 0) {
             numW++;
             isWildcard = true;
         }
     }
+    /* Checks for missing s arguement error. */
     if (numS == 0) {
         priorityError[4] = true;
+    /* Checks for missing r arguement error. */
     } else if (numR == 0) {
         priorityError[5] = true;
     }
+    /* This block is used to check if the wildcard is valid as well making changes to search_text respectively. */
     bool beginWith = false, endWith = false;
     if ((isWildcard) && (!priorityError[4])) {
-        if ((strncmp(search_text, "*", 1) == 0) ^ (strncmp(search_text + strlen(search_text) - 1, "*", 1) == 0)) {
+        if (((strncmp(search_text, "*", 1) == 0) ^ (strncmp(search_text + strlen(search_text) - 1, "*", 1) == 0)) && (countAsterisks(search_text) == 1)) {
             int len = strlen(search_text);
+            /* Store search_text without the begining '*' */
             if (strncmp(search_text, "*", 1) == 0) {
                 endWith = true;
                 if (len > 0) {
@@ -104,6 +128,7 @@ int main(int argc, char *argv[]) {
                     }
                     search_text[len - 1] = '\0';
                 }
+            /* Store search_text without the ending '*' */
             } else {
                 beginWith = true;
                 search_text[len - 1] = '\0';
@@ -112,34 +137,37 @@ int main(int argc, char *argv[]) {
             priorityError[7] = true;
         }
     }
-    //open infile for reading if possible
+    /* Attempts to open the file to read and checks for input file missing. */
     FILE* infile = fopen(argv[argc-2], "r");
-    // Check for error: INPUT_FILE_MISSING 
     if (infile == NULL) {
         priorityError[2] = true;
     }
-    // open outfile for writing if possible
-    FILE* outfile = fopen(argv[argc-1], "w");
-    // Check for error: OUTPUT_FILE_UNWRITABLE  
+     /* Attempts to open the file to write in and checks if it is unwritable. */
+    FILE* outfile = fopen(argv[argc-1], "w"); 
     if (outfile == NULL) {
         priorityError[3] = true;
     }
-
+    /* Calls the handleError method on the most prioritized index of the priorityError array. */
     for (int i = 0; i < 9; i++) {
         if (priorityError[i] == true) {
             return handleError(i);
         }
     }
+    /* Search and replace looping done for both wildcards and simple searches respectively. */
     int curr_line = 1;
     char line[MAX_LINE];
     size_t search_len = strlen(search_text);
+    /* While a line exists store the line as line. */
     while (fgets(line, sizeof(line), infile) != NULL) {
         char *pos, *tmp = line;
+        /* Only replace in the appropriate range if one is given. */
         if ((curr_line >= start_line && curr_line <= end_line) || !hasLine) {
+            /* pos = the first occurence of search_text in tmp */
             while ((pos = strstr(tmp, search_text)) != NULL) {
                 size_t n = pos - tmp;
                 int length = 0;
                 bool isValid = true;
+                /* Shift n based on the wildcard if the search_text found matches the correct wildcard. */
                 if (beginWith || endWith) {
                     if ((endWith && !checkIsEnding(pos, search_text)) || (beginWith && !checkIsBeginning(tmp, pos))) {
                         n += search_len;
@@ -148,31 +176,34 @@ int main(int argc, char *argv[]) {
                     else if (endWith && checkIsEnding(pos, search_text)) {
                         n -= findBeginning(tmp, pos);
                     }
+                    /* Write into file until point of the beginning of search_text. */
                     fwrite(tmp, 1, n, outfile);
-                    char *current = pos;
-                    while (*current != '\0' && !isspace(*current) && !ispunct(*current)) {
-                        current++;
-                        length++;
-                    }
+                    /* Write the replace_text into the file and move pointer based on the length of what needs to be replaced. */
                     if (isValid) {
+                        char *curr_char = pos;
                         fwrite(replace_text, 1, strlen(replace_text), outfile);
-                        tmp = pos + length;
-                    } else {
-                        tmp = pos + search_len;
-                    }
+                        while (*curr_char != '\0' && !isspace(*curr_char) && !ispunct(*curr_char)) {
+                            curr_char++;
+                            length++;
+                        }
+                        search_len = length;
+                    } 
+                /* No wildcard, normal search and replace by writing until the beginning of search_text,
+                write the replace_text into the file, and move the pointer based on the length of search_text. */
                 } else {
                     fwrite(tmp, 1, n, outfile);
                     fwrite(replace_text, 1, strlen(replace_text), outfile);
-                    tmp = pos + search_len;
                 }
+                tmp = pos + search_len;
             }
+            /* Write in the rest of the line into the file. */
             fwrite(tmp, 1, strlen(tmp), outfile);
         } else {
             fputs(line, outfile);
         }
         curr_line++;
     }
-    
+    /* Close both files. */
     fclose(infile);
     fclose(outfile);
     return 0;
